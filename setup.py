76782857a3087fc1e498e8bcd0e7a012e3f25cb0
Chandanabn29* <chandanabn2904@gmail.com>
2026-06-28 18:48:41 +0530

    req added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Chandana B N",
+    author_email="chandanabn2904@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
